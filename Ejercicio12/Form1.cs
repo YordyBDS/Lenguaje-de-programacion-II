c1508447d05ac24deb00835d4f01782563318872
YordyBDS <yordybeltre15@gmail.com>
2025-10-10 18:47:33 -0400

    subiendo formulario 12

---
+++ Ejercicio12/Form1.cs
@@ -0,0 +1,32 @@
+namespace Ejercicio12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float celsius = float.Parse(textBox1.Text);
+            float fahrenheit = (celsius * 9 / 5) + 32;
+            label1.Text = $"Fahrenheit {fahrenheit}";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
